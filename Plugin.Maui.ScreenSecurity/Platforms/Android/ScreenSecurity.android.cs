0dd4fb2f60e8331408951cbffbb8c1062c77e78a
FabriBertani <fabri_bertani@hotmail.com>
2023-07-12 17:48:29 -0300

    Improved Android code by: - Unifying the methods. - Implementing SetRecentsScreenshotEnabled method for Android 13 and above. - Implementing new ErrorsHandler.

--- Plugin.Maui.ScreenSecurity/Platforms/Android/ScreenSecurity.android.cs
+++ Plugin.Maui.ScreenSecurity/Platforms/Android/ScreenSecurity.android.cs
@@ -1,12 +1,11 @@
 ﻿using Android.OS;
 using Android.Views;
+using Plugin.Maui.ScreenSecurity.Handlers;
 
 namespace Plugin.Maui.ScreenSecurity;
 
 partial class ScreenSecurityImplementation : IScreenSecurity
 {
-    private static volatile Handler? handler;
-
     /// <summary>
     /// Prevent screen content from being exposed when the app
     /// is sent to <b>Background</b> or the <b>Recents screen</b>.
@@ -14,25 +13,7 @@ partial class ScreenSecurityImplementation : IScreenSecurity
     /// </summary>
     public void EnableScreenSecurityProtection()
     {
-        try
-        {
-            if (handler?.Looper != Looper.MainLooper)
-                handler = new Handler(Looper.MainLooper!);
-
-            handler?.Post(() =>
-            {
-                var activity = Platform.CurrentActivity;
-
-                activity?.Window?.SetFlags(WindowManagerFlags.Secure, WindowManagerFlags.Secure);
-            });
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"EnableScreenSecurityProtection failed with Exception message: {ex.Message}");
-            Console.WriteLine($"Exception Stacktrace: {ex.StackTrace}");
-            if (ex.InnerException != null)
-                Console.WriteLine($"With InnerException: {ex.InnerException}");
-        }
+        SetScreenSecurityProtection(true);
     }
 
     /// <summary>
@@ -40,24 +21,34 @@ partial class ScreenSecurityImplementation : IScreenSecurity
     /// </summary>
     public void DisableScreenSecurityProtection()
     {
-        try
-        {
-            if (handler?.Looper != Looper.MainLooper)
-                handler = new Handler(Looper.MainLooper!);
+        SetScreenSecurityProtection(false);
+    }
 
-            handler?.Post(() =>
+    private void SetScreenSecurityProtection(bool enabled)
+    {
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            try
             {
                 var activity = Platform.CurrentActivity;
 
-                activity?.Window?.ClearFlags(WindowManagerFlags.Secure);
-            });
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"DisableScreenSecurityProtection failed with Exception message: {ex.Message}");
-            Console.WriteLine($"Exception Stacktrace: {ex.StackTrace}");
-            if (ex.InnerException != null)
-                Console.WriteLine($"With InnerException: {ex.InnerException}");
-        }
+                if (activity != null)
+                {
+#pragma warning disable CA1416
+                    if (Build.VERSION.SdkInt >= BuildVersionCodes.Tiramisu)
+                        activity.SetRecentsScreenshotEnabled(!enabled);
+#pragma warning restore CA1416
+
+                    if (enabled)
+                        activity.Window?.SetFlags(WindowManagerFlags.Secure, WindowManagerFlags.Secure);
+                    else
+                        activity.Window?.ClearFlags(WindowManagerFlags.Secure);
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorsHandler.HandleException(nameof(SetScreenSecurityProtection), ex);
+            }
+        });
     }
 }
