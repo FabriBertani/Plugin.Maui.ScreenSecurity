bee16fbd8107cb8610e76907ede8f77a80a7f200
FabriBertani <fabri_bertani@hotmail.com>
2024-05-25 13:45:10 -0300

    Added Blazor sample.

---
+++ samples/ScreenSecurityBlazorSample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ScreenSecurityBlazorSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
