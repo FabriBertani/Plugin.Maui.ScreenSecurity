de20e976236b06dd8a36a8e297d127585b65ab40
Fabricio Bertani <fabri_bertani@hotmail.com>
2024-10-12 13:06:51 +0200

    Added screen captured event handler implementation on sample app.

--- samples/ScreenSecuritySample/Views/SecondPage.xaml.cs
+++ samples/ScreenSecuritySample/Views/SecondPage.xaml.cs
@@ -21,10 +21,27 @@ public partial class SecondPage : ContentPage
         _screenSecurity.DeactivateScreenSecurityProtection();
 
         isEnabledLabel.Text = $"Screen protection enabled: {_screenSecurity.IsProtectionEnabled}";
+
+        _screenSecurity.ScreenCaptured += OnScreenCaptured;
+    }
+
+    protected override void OnDisappearing()
+    {
+        _screenSecurity.ScreenCaptured -= OnScreenCaptured;
+
+        base.OnDisappearing();
     }
 
     private async void Button_Clicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("..", true);
+    }
+
+    private async void OnScreenCaptured(object sender, EventArgs e)
+    {
+        string title = "ScreenSecuritySample";
+        string message = "Screenshot was taken.";
+
+        await Shell.Current.DisplayAlert(title, message, "Ok");
     }
 }
